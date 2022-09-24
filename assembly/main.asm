bed70b8846f1f45b58f55cc08d160caa5bd7d8e9
朽木不可雕也 <aszswaz@163.com>
2022-09-24 14:57:28 +0800

    添加 C 函数调用规范在汇编中的实现

--- assembly/main.asm
+++ assembly/main.asm
@@ -1,28 +1,30 @@
-section .bss
-resb 64
+; 声明此函数在别的文件中
+extern print
+; 声明程序入口，只能是 _start
+global _start
 
 ; 自定义数据段，未使用“传统”的数据段 .data
-section maindata
+section .data
 ; 在数据段中存放一个字符串，OA 是一个换行符
 strHello db "Hello World!", 0AH
 STRLEN equ $ - strHello
 
-; 自定义代码段，未使用“传统”的代码段 .txt
-section maintext
-; 声明此函数在别的文件中
-extern print
-
-; 声明程序入口，只能是 _start
-global _start
-
+section .text
 _start:
-    ; 传入参数，字符串的长度
-    push STRLEN
+    ; 创建栈帧
+    push ebp
+    mov ebp, esp
+
     ; 传入参数，待打印的字符串
     push strHello
+    ; 传入参数，字符串的长度
+    push STRLEN
     ; 调用外部函数
     call print
-    ; 函数调用完毕，继续执行
+    ; 回收 print 函数的参数占用的空间
+    add esp. 8
+
+    ; 函数调用完毕，退出程序
     ; 设置程序返回值（exit code）
     mov ebx, 0
     ; 设置系统子功能调用号，1: sys_exit
--- assembly/print.asm
+++ assembly/print.asm
@@ -1,26 +1,29 @@
-section .text
-mov eax, 0x10
-jmp $
-
-; 自定义数据段
-section printdata
-num db 3
-
-; 自定义代码段
-section printtext
-
 ; 导出 print，供其他模块使用
 global print
 
+; 自定义代码段
+section .text
 print:
-    ; 字符串长度
-    mov edx,[esp+8]
-    ; 设置要输出的字符串
-    mov ecx,[esp+4]
+    ; 创建栈帧
+    ; 备份上一层函数的堆栈地址
+    push ebp
+    ; 将 ebp 更新为当前对战地址
+    mov ebp, esp
 
-    mov ebx, 1
+    ; 字符串长度
+    mov edx,[ebp+8]
+    ; 设置要输出的字符串
+    mov ecx,[ebp+12]
+
     ; 设置系统子功能号，4:sys_write
     mov eax, 4
     ; 开始系统调用
     int 0x80
+
+    ; 即将离开函数，销毁栈帧
+    ; 跳过局部变量的空间（如果有）
+    ; mov esp, ebp
+    ; pop ebp
+    ; 上述两条指令可以使用 leave 代替
+    leave
     ret

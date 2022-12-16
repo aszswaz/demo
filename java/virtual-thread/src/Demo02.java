ee8c0a1fb7613cceb3d26a20ff1608b3836e458a
朽木不可雕也 <aszswaz@163.com>
2022-12-16 23:51:38 +0800

    backup

--- java/virtual-thread/src/Demo02.java
+++ java/virtual-thread/src/Demo02.java
@@ -3,7 +3,7 @@ import java.util.concurrent.Executors;
 @SuppressWarnings({"StatementWithEmptyBody", "LoopConditionNotUpdatedInsideLoop"})
 public class Demo02 {
     /**
-     * 向虚拟线程的调度器添加一定数量的虚拟线程，并且使用死循环让虚拟线程无法终止
+     * 测试当虚拟线程进行 BIO 操作或 sleep() 时，是否真的切换到其他的虚拟线程去运行了
      */
     public static void main(String[] args) throws InterruptedException {
         // 使用自行创建的调度线程运行虚拟线程
@@ -11,8 +11,16 @@ public class Demo02 {
             for (int i = 0; i < 10; i++) {
                 int num = i;
                 service.submit(() -> {
-                    System.out.println("i: " + num);
-                    while (args.length == 0){}
+                    try {
+                        System.out.println("i: " + num);
+                        // BIO 操作有些麻烦，这里用 sleep 代替，暂停当前虚拟线程，执行其他虚拟线程
+                        Thread.sleep(5000);
+                        // 由于死循环的存在，只有部分虚拟线程可以继续执行，其他虚拟线程会永远的处于 sleep 状态
+                        System.out.println("wait, i: " + num);
+                        while (args.length == 0) {}
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 });
             }
         }

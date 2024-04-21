9fd177e8660678247af2aa2df6184b97ad7c1c72
aszswaz <aszswaz@163.com>
2024-04-21 10:24:31 +0800

    调整目录结构

---
+++ linux/bluetooth/python/rfcomm/rfcomm-service.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import socket
+
+
+def main():
+    port = 28
+    server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    server_sock.bind((socket.BDADDR_ANY, port))
+    server_sock.listen(1)
+
+    client_sock, addr = server_sock.accept()
+
+    server_sock.close()
+    pass
+
+
+main()
